1729b0ad38ba70dcf39e91d4d08d7fa26cb72df9
ferchuqui <luisfernandofarji@hotmail.com>
2020-01-11 16:10:01 -0300

    Capitulo 19 Adding Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => (
@@ -16,9 +17,13 @@ const App = () => (
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
       </Carousel>
     </Categories>
+    <Footer/>
   </div>
+
 );
 
 export default App;
